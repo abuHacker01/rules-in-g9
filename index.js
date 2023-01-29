8581da855f131e040dfe19172e00c3b1ee2854b8
abuhacker01 <abuhacker012@gmail.com>
2023-01-29 16:16:27 +0500

    all rules

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 
 // GitHub
 
-/// progromist bo'lmaganlar ma'lumotini fleshkada tarqatadi progromistlar GitHub orqali 
+/// progromist bo'lmaganlar ma'lumotini fleshkada tarqatadi progromistlar GitHub orqali
 
 /// ma'lumot ulashishda ishlaydigan nomlar
 
@@ -66,7 +66,7 @@
 /// 8 ta data type bor.....=> 2 ga bolinadi ==> primitive vs non-primitive ;
 
 /// primetive : malum bir informatsiyalarni o'zida saqlaydi .
-/// non-primetive : bir 
+/// non-primetive : bir
 
 
 //// primitive :
@@ -90,3 +90,67 @@
 //// non-primitive :
 
 ///// object...............=> array, function;
+
+////////////// uchinchi dars
+
+// Type Convirsion
+
+/// Type Convirsion-bu ma'lum data turidagi ma'lumotni boshqa data turiga o'zgartirish\
+
+/// BOOLEAN ==> STRING
+
+// first
+
+// let valeu = true;     // yangi o'zgaruvchhi ochildi ;
+// console.log(typeof valeu); // bu yerda boolean chiqadi ;
+// valeu = String(valeu);  // shu yerda stringga o'zgaryapti ;
+// console.log(typeof valeu);  // string-deb chiqadi ;
+
+
+// STRIN ==> NUMBER
+
+// let str = "123";
+// console.log(typeof str);    // string ;
+// str = Number(str);          // shu yerda Numberga o'tdi ;
+// console.log(typeof str);    // Number chiqadi ;
+
+/// agar string ustida (+)-dan tashqari barcha arifmetik amal bajarsaxam Numberga o'tadi
+
+// console.log("2" / "2");          |
+// console.log("2" * "2");          |____ bu xolatlarda
+// console.log("2" - "2");          |
+
+/// agar stringni oldiga (+) qo'yib qo'ysaxam Numberga o'tadi ;
+
+/// agar hariflarni ustida arifmetik amal bajarsak NaN chiqadi ;
+
+// Basic operators , maths :
+
+/// 1. +.......................=> qo'shuv
+/// 2. -.......................=> ayruv
+/// 3. *.......................=> ko'paytiruv
+/// 4. /.......................=> bo'luv
+/// 5. %.......................=> foizlig bo'luv
+/// 6. **......................=> kvadrat
+
+// &&..........................=> (--va--) belgisi :
+
+/// bu belgi bir tomonidaxam false bo'lsa (false) qaytaradi ;
+/// faqat ikkala tomonida true bo'lsa kegin (true) qaytaradi ;
+
+
+// ||..........................=> (--yoki--) belgisi :
+
+/// bu belgi bir tomonidaxam true bo'lsa (true) qaytaradi ;
+/// faqat ikkala tomonida false bo'lsa kegin (false) qaytaradi ;
+
+// ==..........................=> teng yoki yo'qligini aytadi .
+
+/// misol uchun :
+
+//// console.log("1" == 1);      // true
+
+// ===.........................=> qat'iy tenglig :
+/// uni oddiy tenglikdan farqi
+
+// \n..........................=> terminalda bitta "Enter" tashlab beradi ;
