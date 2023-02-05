f43e07a03561911d348e0a12f8be12bb7de956f5
abuhacker01 <abuhacker012@gmail.com>
2023-02-05 19:58:14 +0500

    + works

--- index.js
+++ index.js
@@ -391,3 +391,15 @@
 //// functionlar hoisting bo'ladi .
 //// chunki javascript engine yurganda birinchi bo'lib function keyvordlarini o'qib oladi ;
 
+/// call :
+
+//// call - bu Functionimizni chaqirish ;
+//// call - agar Functionimizni chaqirmasak u ishlamaydi ;
+
+/// let/const o'z hususiyatini yo'qotishi :
+
+//// agar globalda bitta let orqali o'zgaruvchi ochib va functionni ichidaxam
+///// globaldagi bilan name-si bir xil verable ochsak xato ko'rsatmaydi ;
+
+////// va functionni chaqirsag functionni ichidagi verable chiqadi ;
+
