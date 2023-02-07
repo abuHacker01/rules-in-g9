8bd59c438b58e92eb430aa0e314072c40f8caed4
abuhacker01 <abuhacker012@gmail.com>
2023-02-07 16:25:09 +0500

    sixth lesson

--- index.js
+++ index.js
@@ -348,7 +348,7 @@
 /////////////////////// oltinchi dars
 
 
-// Function :
+// Function declaration :
 
 /// Function bu javascriptdagi dastur kod bloci ;
 
@@ -381,39 +381,8 @@
 //     console.log("xar xil amallar");
 // }
 
+/// default parametr :
 
-// Function :
-
-/// Function bu javascriptdagi dastur kod bloci ;
-
-/// Dinamic function :
-
-//// functionlar hoisting bo'ladi .
-//// chunki javascript engine yurganda birinchi bo'lib function keyvordlarini o'qib oladi ;
-
-/// call :
-
-//// call - bu Functionimizni chaqirish ;
-//// call - agar Functionimizni chaqirmasak u ishlamaydi ;
-
-/// let/const o'z hususiyatini yo'qotishi :
-
-//// agar globalda bitta let orqali o'zgaruvchi ochib va functionni ichidaxam
-///// globaldagi bilan name-si bir xil verable ochsak xato ko'rsatmaydi ;
-
-////// va functionni chaqirsag functionni ichidagi verable chiqadi ;
-
-/// return :
-
-//// returnni yonida nima turgan bo'lsa osha functionni qiymati bo'ladi ;
-//// return function-ni eng oxirida yoziladi va undan kegin yozilgan xar qanday kod ishlamaydi ;
-
-
-/// sytax of function :
-
-// function functionname(parametrs) {
-//     console.log("xar xil amallar");
-// }
 
 // let num1 = 1;
 // let num2 = 1;
