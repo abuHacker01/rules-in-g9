ab0afbf0031661c6d4345eac64528cc289dffff7
abuhacker01 <abuhacker012@gmail.com>
2023-02-08 20:23:58 +0500

    works

--- index.js
+++ index.js
@@ -526,3 +526,41 @@
 //     function (yes) { console.log("You agreed."); },
 //     function (no) { console.log("You canceled the execution."); }
 // );
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// mandarinni axlatga tashlash
+
+/// let user = {};
+/// user.name = "John";
+/// user.surname = "Smith";
+/// user.name = "Pate"
+/// delete user.name;
+
+// objectni bo'sh-bo'shemasligini tekshirib beradi
+
+/// function isEmpty(test) {
+///     if (test === {}) {
+///         return true;
+///     } else {
+///         return false;
+///     }
+/// }
+
+/// console.log(isEmpty(user))
+
