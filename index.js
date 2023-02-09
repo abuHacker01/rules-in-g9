ea8e847ac2751b2b44032094c6ff197c7450527f
abuhacker01 <abuhacker012@gmail.com>
2023-02-09 21:02:36 +0500

    works

--- index.js
+++ index.js
@@ -600,3 +600,24 @@
 ///     res = valeus[2];
 /// }
 /// console.log(res);
+
+// mandarinni axlatga tashlash
+
+/// let user = {};
+/// user.name = "John";
+/// user.surname = "Smith";
+/// user.name = "Pate"
+/// delete user.name;
+
+// objectni bo'sh-bo'shemasligini tekshirib beradi
+
+/// function isEmpty(test) {
+///     if (test === {}) {
+///         return true;
+///     } else {
+///         return false;
+///     }
+/// }
+
+/// console.log(isEmpty(user))
+
