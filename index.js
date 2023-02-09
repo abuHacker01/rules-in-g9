77c6c660a5d0d7dcb5ad6b72fc99dae0dc733a06
abuhacker01 <abuhacker012@gmail.com>
2023-02-09 21:03:02 +0500

    = works

--- index.js
+++ index.js
@@ -621,3 +621,46 @@
 
 /// console.log(isEmpty(user))
 
+// eng katta oylig
+
+/// let obj = {
+///     Ann: Math.round(Math.random() * 1000),
+///     Joxn: Math.round(Math.random() * 1000),
+///     Pete: Math.round(Math.random() * 1000)
+/// };
+/// console.log(obj);
+/// let valeus = Object.values(obj);
+/// let res = 0;
+/// if (valeus[0] > res) {
+///     res = valeus[0];
+/// } if (valeus[1] > res) {
+///     res = valeus[1];
+/// } if (valeus[2] > res) {
+///     res = valeus[2];
+/// }
+/// console.log(res);
+
+/// if (valeus[0] > valeus[1] && valeus[0] < valeus[2] || valeus[0] < valeus[1] && valeus[0] > valeus[2]) {
+///     res = valeus[0];
+/// } if (valeus[1] > valeus[0] && valeus[1] < valeus[2] || valeus[1] < valeus[0] && valeus[1] > valeus[2]) {
+///     res = valeus[1];
+/// } if (valeus[2] > valeus[1] && valeus[2] < valeus[0] || valeus[2] < valeus[1] && valeus[2] > valeus[0]) {
+///     res = valeus[2];
+/// }
+/// console.log(res);
+
+/// if (valeus[0] < res) {
+///     res = va leus[0];
+/// } if (valeus[1] < res) {
+///     res = valeus[1];
+/// } if (valeus[2] < res) {
+///     res = valeus[2];
+/// }
+/// console.log(res);
+
+// let menu = {
+//     width: 200,
+//     height: 300,
+//     title: "My menu"
+// };
+// let valeus = Object.values(menu);
