ac3160885781e591d456ba9791f2f28ba1dc475e
abuhacker01 <abuhacker012@gmail.com>
2023-02-13 20:15:36 +0500

    works

--- index.js
+++ index.js
@@ -663,3 +663,31 @@
 // }                                           |
 // let user = makeUser()                       |
 // console.log(user.ref.name);                 |
+
+
+// this 
+
+/// this bu thini o'rab turgan functionni o'rab turgan object
+//// misol uchun :
+
+// let object = {
+//     name: "Dilmurod",
+//     surname: "Xabibullaev",
+//     fn() {
+//         console.log(this.name + this.surnam);
+//     }
+// };
+// console.log(object.fn);
+
+/// qachon this kerak ?
+//// qachon oddiy usulda korsatsak keginchalik objectni nomini o'zgartirsak bizning usul ishlamaydi ;
+
+/// qachon this ishlamaydi ?
+
+//// qachonki arrow function ishlasa
+
+
+// garberge collaction :
+
+/// garberge collectio b qachonki non-primetive data turilig verable yaratsag
+//// va uni primetive-ga o'tkazsag stackdagi malumot bilan heapdagi malumoni bog'lab turuvchi ko'prig uzuladi va heapdagi malumot garbergega tushadi ;
