398700cf8135aa64c6302fba9585fd7c80ad6b7c
abuhacker01 <abuhacker012@gmail.com>
2023-03-17 19:50:59 +0500

    works

--- index.js
+++ index.js
@@ -154,7 +154,7 @@
 //// console.log("1" == 1);      // false
 
 /// uni oddiy tenglikdan farqi u data typesigaxam qaraydi ;
- 
+
 
 // if , else :
 
@@ -408,3 +408,179 @@
 // structuredClone :
 
 /// structuredClone bu objectni faqat qiymatlarini copy qilish ;
+
+
+
+
+
+// toq va juft sonlarni chiqarib
+
+
+// for (i = 3; i < 100; i++) {
+//     if (i%2==1) {
+//         console.log(i);
+//     }else{
+//         console.log(i);
+//     }
+// }
+
+// New Date :
+
+/// New Date-bizga kamputerdagi vaqtni chiqarib beradi :
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// JSON :
+
+/// JSON-ning kengaytmasi : JavaScript Object Notation
+
+/// JSON-ning vazifasi :
+
+//// bizing o'zgaruvchilar boshqa programming language lar bilan to'g'ri kelmasligi mumkun .
+///// shuning uchun bizga JSON-kerak ;
+
+//// biz beckend-da malumot oldi-berdilari uchunxam JSON-dan foidalanamiz
+
+/// JSON-daxam xuddi JavaScript-ga o'xshab extncioni bo'ladi ;
+
+/// JSON-ni syntaxi boshqa faylda :
+
+//// uni objectdan farqi :
+///// uni keylarixam stringda yoziladi ;
+
+//// uni faqat ikkitalig qo'shtirnoqda yozish shart . Budegani bittalig qo'shtirnoda yozsa xato beradi
+
+
+/// Metods :
+
+//// add :
+
+///// stringfy...........=> objectni keylarini string ichida yozib beradi ;
+
+//// exec :
+
+///// agar iz uni ichidagi bironta malumotni chiqarmoxchi bo'lsag
+////// oddiy nuqtabilan chiqara olmaymiz . Uning uchun :
+
+///// parse.............=> bitta parametr qaysi JSON-ga kirishi misol uchun :
+
+////// let obj = { name: "Dilmurod", surname: "Xabibullaev", age: 15, work: false };                    |
+////// let res = JSON.stringify(obj);                                                                   |---------bu yerda 15-chiqadi ;
+////// console.log(JSON.parse(res).age);                                                                |
+
+//// Agar biz backentga faqat o'zimiz xoxlgan malumotlarni jo'natmoxchi bo'lsag :
+
+///// let obj = { name: "Dilmurod", surname: "Xabibullaev", age: 15, work: false };                     |
+///// let res = JSON.stringify(obj, ["name"]);                                                          |---------bu yerda (--{"name":"Dilmurod"}--)chiqadi
+///// console.log(res);                                                                                 |
+
+///// buni sababi ikkinchi parametrda jo'natmoxchi bo'lgan key-ni yozganimiz ;
+
+//// bizda uchinchi parametrxam bor :
+
+// let obj = { name: "Dilmurod", surname: "Xabibullaev", age: 15, work: false };       |
+// let res = JSON.stringify(obj, ["name", "surnam", "age", "work"],2);                 |---------u bizda (--name/surname/age/work--)ni oldidan nechta probel bo'lishini elon qiladi
+// console.log(res);                                                                   |
+
+//{
+//    name: 'Dilmurod',
+//    surname: 'Xabibullaev',
+//    age: 15,
+//    work: false,
+//}
+
+
+
+// map , set :
+
+/// Sizda qanaqa malumot turi bo'lishidan qat'iy nazar xuddi anashu malumot turini saqlab qo'yadi ;
+//// map va set o'zimizning ojectga o'xshaydi . Misol uchun :
+
+// let map = new Map();                |
+// let obj = Object();                 |_________bu yerda : (Map(0) {})___chiqadi
+// console.log(map);                   |                    ({}       )
+// console.log(obj);                   |
+
+///// ularning farqi :
+
+////// mapda (--Map(0)--)- bor
+
+/// agar biz map-ni to'liq hususiatlarini ishlatishimiz uchun ularni metodlaridan foidalanishimiz kerak :
+
+/// Metods :
+
+//// set................=> malumot qo'shish uchun . Ikkita qiymat qbul qiladi : 1.key , 2.value ;
+
+///// objectdan farqi :
+////// objectda key va valuelarni (--:--)-bilab ajratadi . map-da esa (--=>--)-bilan
+//// get
+//// delete
+//// clear
+
+// Scope
+
+/// scope-lar xar xil bo'ladi :
+
+//// function scope
+//// while scope
+//// if&else scope
+//// for loop scope...
+
+/// scopelarni ishlashi :
+
+//// aslida javacriptda yozgan kodlarimiz bitta global scope-da joylashadi :
+///// ayni JavaScript file ochishimiz bilan uni ichida ko'rinms scope ochiladi ;
+
+/// scope-ni ko'rinishi :
+
+// let a = 2;
+// {
+//     let a = 1;
+// }
+// console.log(a);   /// bu yerda ikki chiqadi chunki 1-degan o'zgaruvchi scope ichida ;
+
+
+// let q = 2;
+// {
+//     console.log(q);     /// bu yerda ikki chiqadi cchunki scope-da chiqish bor kirish yo'q;
+// }
+
+// Lexical Environment :
+
+/// qayerda scope bo'lsa o'sha yerni o'zini (--Lexical Environment--)-bo'ladi ;
+
+/// Lexical Environment ikkiga bo'linadi :
+
+//// 1. recorde 
+//// 1. reference
+
+/// qachonki biz misol uchun console.log qilsag :
+
+//// birinchi bo'lib recordega qaraydi . Agar topa olmasa kegin reference-dan qidiradi .           | shu xodisani (--Lexical Environment--) deyiladi ;
+
+
+// Closure function :
+
+
+/// Closure function-deb qachonki function o'zini scope-sidan tashqaridagi o'zgaruvchini ishlatsa aytadi ;
+
+/// misol uchun :
+
+// let number =12;                       |
+// function name(num) {                  |
+//     console.log(num);                 |__________shu (--Closure function--) deyiladi ;
+// }                                     |
+// name(number);                         |
+
+// Array.prototype.length
